f9eceed7352909c307c913fc9067d941b41c9f38
Sumi-westmin <w2095779@westminster.ac.uk>
2026-05-25 09:31:38 +0100

    Add auth views - stubbed data

---
+++ LogOutView.swift
@@ -0,0 +1,18 @@
+//
+//  LogOutView.swift
+//  Event_Manager_Demo
+//
+//  Created by Sumi Sastri on 22/05/2026.
+//
+
+import SwiftUI
+
+struct LogOutView: View {
+    var body: some View {
+        Text("Logout")
+    }
+}
+
+#Preview {
+    LogOutView()
+}
